2a2d6f064dcfdfb70df76fd7696910cc72d2fc94
salewis38 <46199139+salewis38@users.noreply.github.com>
2024-03-03 20:47:58 +0000

    Improved robustness
    Added overall "try" to prevent crash if network unavailable

--- solaxtopvoutput.py
+++ solaxtopvoutput.py
@@ -11,7 +11,7 @@ import requests
 
 # MIT License
 #
-# Copyright (c) 2021 tiki1980
+# Copyright (c) 2024 tiki1980, Steve Lewis
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -33,9 +33,10 @@ import requests
 
 # Changelog:
 # v0.2    10/Feb/24 Re-factored as a single file, added consumption data. Pylint score = 10/10
-# v0.3    10/Feb/24 added test for valid consumption data
+# v0.3    10/Feb/24 Aded test for valid consumption data
+# v0.4    03/Mar/24 Added overall "try" to prevent crash if network is unreachable
 
-VERSION = "v0.3"
+VERSION = "v0.4"
 
 def jprint(obj):
     """ print json object for debugging purposes"""
@@ -50,14 +51,18 @@ def get_real_time_solax_data(token, registration_nr):
     query = "?" + urlencode({"tokenId": token, "sn": registration_nr})
     api_url =  urljoin(api_url, query)
 
-    logger.debug("Api url: %s", str(api_url))
+    dummy_response = {"success":False,"exception":"Query failure!","result":{"inverterSN":"","sn":"","acpower":0,"yieldtoday":0,"yieldtotal":0,"feedinpower":0,"feedinenergy":0,"consumeenergy":0,"feedinpowerM2":0,"soc":0,"peps1":0,"peps2":0,"peps3":0,"inverterType":"4","inverterStatus":"0","uploadTime":"1970-01-01 00:00:00","batPower":0,"powerdc1":0,"powerdc2":0,"powerdc3":0,"powerdc4":0,"batStatus":0},"code":0}
+
+    logger.debug("Api url: %s", str(api_url))down
     try:
         response = requests.get(api_url, timeout = 10)
         response.raise_for_status()
     except requests.exceptions.HTTPError:
         logger.exception("HTTP Error")
+        return dummy_response
     except requests.exceptions.RequestException:
         logger.exception("Exception occurred")
+        return dummy_response
     logger.debug("HTTP Response status code: %s", str(response.status_code))
     logger.info(str(response.text))
     return response.json()
@@ -91,7 +96,7 @@ if __name__ == '__main__':
 
     # Parse YAML configuration file
     with open(os.path.join(os.path.dirname(__file__),"config.yml"), "r", encoding='UTF-8') \
-        as yamlfile:
+        as yamlfile:down
         try:
             config = yaml.load(yamlfile, Loader=yaml.FullLoader)
         except yaml.YAMLError as err:
@@ -111,50 +116,55 @@ if __name__ == '__main__':
     logging.info("Started SolaxToPVOutput, version: %s", VERSION)
 
     while True:
-        # Request latest data from SolaxCloud
-        SolaxData = get_real_time_solax_data(config["SolaxCloud"]["tokenId"], \
-            config["SolaxCloud"]["registrationNr"])
+        try:
+            # Request latest data from SolaxCloud
+            SolaxData = get_real_time_solax_data(config["SolaxCloud"]["tokenId"], \
+                config["SolaxCloud"]["registrationNr"])
 
-        # If latest reading is successful, parse data and upload to PVOutput
-        if SolaxData["success"] is True:
-            # Create 5-minute payload data for PVOutput
-            # Useful fields in SolaxData (with sample data):
-            # "acpower":2670.0,  # Instantaneous ac power generated (W)
-            # "yieldtoday":6.1,  # Generation energy today (kWh)
-            # "yieldtotal":1365.3,  # Lifetime generation energy
-            # "feedinpower":2527.0,  # Instantaneous export power (W)
-            # "feedinenergy":9.64,  # Export energy today (kWh)
-            # "consumeenergy":22.2,  # Lifetime import energy (while running)
+            # If latest reading is successful, parse data and upload to PVOutput
+            if SolaxData["success"] is True:
+                # Create 5-minute payload data for PVOutput
+                # Useful fields in SolaxData (with sample data):
+                # "acpower":2670.0,  # Instantaneous ac power generated (W)
+                # "yieldtoday":6.1,  # Generation energy today (kWh)
+                # "yieldtotal":1365.3,  # Lifetime generation energy
+                # "feedinpower":2527.0,  # Instantaneous export power (W)
+                # "feedinenergy":9.64,  # Export energy today (kWh)
+                # "consumeenergy":22.2,  # Lifetime import energy (while running)
 
-            gen_tot_wh: int = SolaxData["result"]["yieldtotal"] * 1000  # v1 = Lifetime energy gen
-            gen_pwr: int = SolaxData["result"]["acpower"]  # v2 = Power generation
+                gen_tot_wh: int = SolaxData["result"]["yieldtotal"] * 1000  # v1 = Lifetime energy gen
+                gen_pwr: int = SolaxData["result"]["acpower"]  # v2 = Power generation
 
-            cons_tot_wh: int = 0
-            con_pwr: int = 0
-            if SolaxData["result"]["consumeenergy"] is not None:
-                cons_tot_wh = SolaxData["result"]["consumeenergy"] * 1000 # v3 = Energy consumption
-            if SolaxData["result"]["feedinpower"] is not None:
-                con_pwr = max(gen_pwr - SolaxData["result"]["feedinpower"], 0)  # v4 = Power cons
+                cons_tot_wh: int = 0
+                con_pwr: int = 0
+                if SolaxData["result"]["consumeenergy"] is not None:
+                    cons_tot_wh = SolaxData["result"]["consumeenergy"] * 1000 # v3 = Energy consumption
+                if SolaxData["result"]["feedinpower"] is not None:
+                    con_pwr = max(gen_pwr - SolaxData["result"]["feedinpower"], 0)  # v4 = Power cons
 
-            upld_date_time = SolaxData["result"]["uploadTime"]
-            upld_date = upld_date_time[0:10].replace('-','')
-            upld_time = upld_date_time[11:16]
+                upld_date_time = SolaxData["result"]["uploadTime"]
+                upld_date = upld_date_time[0:10].replace('-','')
+                upld_time = upld_date_time[11:16]
 
-            pvoutput_payload = {
-                'd': str(upld_date),
-                't': str(upld_time),
-                'v1': str(gen_tot_wh),
-                'v2': str(gen_pwr),
-                'v3': str(cons_tot_wh),
-                'v4': str(con_pwr),
-                'c1': "1"
-            }
+                pvoutput_payload = {
+                    'd': str(upld_date),
+                    't': str(upld_time),
+                    'v1': str(gen_tot_wh),
+                    'v2': str(gen_pwr),
+                    'v3': str(cons_tot_wh),
+                    'v4': str(con_pwr),
+                    'c1': "1"
+                }
 
-            pvoutput = upload_to_pvoutput(pvoutput_payload)
+                pvoutput = upload_to_pvoutput(pvoutput_payload)
 
-        else:
-            logger.error("SolaxCloud returned failure in Json")
-            logger.error(str(SolaxData))
+            else:
+                logger.error("SolaxCloud returned failure in Json")
+                logger.error(str(SolaxData))
 
-        logger.debug("Sleeping for 5 minutes")
-        time.sleep(300)
+        except Exception as error:
+            logger.error(str(error))
+
+        finally:
+            logger.debug("Sleeping for 5 minutes")
+            time.sleep(300)
